948f6d26bc60c24cbcace3935ecc7a47a90aa023
Ian Kazlauskas <nayzak90@gmail.com>
2012-02-09 21:26:42 +1000

    fixed shit code

--- application/forms/create_admin.py
+++ application/forms/create_admin.py
@@ -7,13 +7,8 @@ class CreateAdminForm(Form):
 
     email = TextField(u'E-mail', [validators.required(), validators.email()], description=u'E-mail будет использован в качестве логина')
     password = PasswordField(u'Пароль', [validators.required(), validators.length(min=6)])
-    password_confirmation = PasswordField(u'Подтвердите пароль', [validators.required()])
+    password_confirmation = PasswordField(u'Подтвердите пароль', [validators.required(), validators.equal_to('password')])
     last_name = TextField(u'Фамилия', [validators.required()])
     first_name = TextField(u'Имя', [validators.required()])
     middle_name = TextField(u'Отчество', [validators.required()])
     submit = SubmitField(u'Создать администратора')
-
-    def __init__(self, **kwargs):
-        super(CreateAdminForm, self).__init__(**kwargs)
-        #добавляем валидацию на соответствие пароля и подтверждение
-        self.password_confirmation.validators += [validators.equal_to('password')]
